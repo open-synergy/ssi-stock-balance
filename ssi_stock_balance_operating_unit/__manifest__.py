800e3256f058192bdae4f95fb91e9039c94b0183
Andhitia Rama <andhitia.r@gmail.com>
2026-07-17 18:52:00 +0700

    [ADD] ssi_stock_balance_operating_unit
    Modul glue baru yang menambahkan integrasi Operating Unit pada model
    stock_balance (modul basis ssi_stock_balance), mengikuti pola baku
    glue module Operating Unit SSI: field operating_unit_id via mixin
    mixin.single_operating_unit, security (res.groups + ir.rule), dan
    penyesuaian view tree/form/search.

---
+++ ssi_stock_balance_operating_unit/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Balance + Operating Unit",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "contributors": [
+        "Andhitia Rama <andhitia.r@gmail.com>",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_stock_balance",
+        "ssi_operating_unit_mixin",
+    ],
+    "data": [
+        "security/res_group/stock_balance.xml",
+        "security/ir_rule/stock_balance.xml",
+        "view/stock_balance.xml",
+    ],
+}
